384310478b1cb496dbd112eb7412a6059c8cd07c
aleen <aleenawa@gmailcom>
2025-05-21 09:29:47 +0300

    hi

--- app/src/main/java/com/example/alinfinal/searchActivity.java
+++ app/src/main/java/com/example/alinfinal/searchActivity.java
@@ -1,4 +1,4 @@
-package com.example.alinfinal.data;
+package com.example.alinfinal;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.alinfinal.R;
 
 public class searchActivity extends AppCompatActivity {
 
